dceaa204a9a67fb3fd09c00808e9f2c084928a23
usfuprog <vitspa@centrum.cz>
2016-04-09 10:57:17 +0200

    procedure.sql

---
+++ SQL/procedure.sql
@@ -0,0 +1,24 @@
+DROP PROCEDURE IF EXISTS nacist_podkategorie;
+delimiter $$;
+
+CREATE PROCEDURE nacist_podkategorie(IN startid INT, OUT n INT)
+BEGIN
+DECLARE pocet, poradi INT DEFAULT 0;
+DECLARE katNazev VARCHAR(60);
+DROP TABLE IF EXISTS __podkat;
+CREATE TEMPORARY TABLE __podkat
+(poradi INT, uroven INT, id INT, katNazev VARCHAR(60))
+ENGINE=HEAP;
+
+SELECT COUNT(*) FROM categories WHERE id=startid INTO pocet;
+
+IF pocet=1 THEN
+SELECT name FROM categories WHERE id=startid INTO katNazev;
+INSERT INTO __podkat VALUES(0, 0, startid, katNazev);
+
+END IF;
+
+SELECT COUNT(*) FROM __podkat INTO n;
+END
+
+SHOW PROCEDURE STATUS;
